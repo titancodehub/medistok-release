c4f678df2f9f71f4a6dfde6567ecbf5a07f5750c
titanabrian <titanabrian.05@gmail.com>
2026-01-17 09:16:48 +0100

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/medistok-release/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
